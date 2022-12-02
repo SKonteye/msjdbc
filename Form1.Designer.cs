a58b47f351abc114d2c63d252886cf91d74b1992
Skonteye <98233015+SKonteye@users.noreply.github.com>
2022-12-02 20:29:01 +0000

    Adding files to Git

---
+++ Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace GestionCouture
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtgView = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNom = new System.Windows.Forms.TextBox();
+            this.txtPrenom = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTelephone = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtAdresse = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.cbbGenre = new System.Windows.Forms.ComboBox();
+            this.btnQuitter = new System.Windows.Forms.Button();
+            this.btnLister = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dtgView
+            // 
+            this.dtgView.BackgroundColor = System.Drawing.Color.White;
+            this.dtgView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgView.Location = new System.Drawing.Point(4, 219);
+            this.dtgView.Name = "dtgView";
+            this.dtgView.Size = new System.Drawing.Size(784, 213);
+            this.dtgView.TabIndex = 0;
+            this.dtgView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtgView_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.PeachPuff;
+            this.label1.Location = new System.Drawing.Point(92, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nom";
+            // 
+            // txtNom
+            // 
+            this.txtNom.Location = new System.Drawing.Point(94, 56);
+            this.txtNom.Name = "txtNom";
+            this.txtNom.Size = new System.Drawing.Size(100, 20);
+            this.txtNom.TabIndex = 2;
+            this.txtNom.TextChanged += new System.EventHandler(this.txtNom_TextChanged);
+            // 
+            // txtPrenom
+            // 
+            this.txtPrenom.Location = new System.Drawing.Point(94, 117);
+            this.txtPrenom.Name = "txtPrenom";
+            this.txtPrenom.Size = new System.Drawing.Size(100, 20);
+            this.txtPrenom.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.PeachPuff;
+            this.label2.Location = new System.Drawing.Point(99, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Prenom";
+            // 
+            // txtTelephone
+            // 
+            this.txtTelephone.Location = new System.Drawing.Point(262, 56);
+            this.txtTelephone.Name = "txtTelephone";
+            this.txtTelephone.Size = new System.Drawing.Size(100, 20);
+            this.txtTelephone.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.PeachPuff;
+            this.label3.Location = new System.Drawing.Point(260, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Telephone";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.PeachPuff;
+            this.label4.Location = new System.Drawing.Point(267, 89);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Genre";
+            // 
+            // txtAdresse
+            // 
+            this.txtAdresse.Location = new System.Drawing.Point(430, 117);
+            this.txtAdresse.Multiline = true;
+            this.txtAdresse.Name = "txtAdresse";
+            this.txtAdresse.Size = new System.Drawing.Size(126, 49);
+            this.txtAdresse.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.PeachPuff;
+            this.label5.Location = new System.Drawing.Point(435, 89);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Adresse";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(430, 56);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(100, 20);
+            this.txtEmail.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.PeachPuff;
+            this.label6.Location = new System.Drawing.Point(428, 28);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(37, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Email";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.PeachPuff;
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdd.Location = new System.Drawing.Point(636, 64);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 13;
+            this.btnAdd.Text = "&Ajouter";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // cbbGenre
+            // 
+            this.cbbGenre.FormattingEnabled = true;
+            this.cbbGenre.Items.AddRange(new object[] {
+            "Homme",
+            "Femme"});
+            this.cbbGenre.Location = new System.Drawing.Point(262, 115);
+            this.cbbGenre.Name = "cbbGenre";
+            this.cbbGenre.Size = new System.Drawing.Size(100, 21);
+            this.cbbGenre.TabIndex = 14;
+            // 
+            // btnQuitter
+            // 
+            this.btnQuitter.BackColor = System.Drawing.Color.PeachPuff;
+            this.btnQuitter.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnQuitter.Location = new System.Drawing.Point(636, 167);
+            this.btnQuitter.Name = "btnQuitter";
+            this.btnQuitter.Size = new System.Drawing.Size(75, 23);
+            this.btnQuitter.TabIndex = 15;
+            this.btnQuitter.Text = "Quitter";
+            this.btnQuitter.UseVisualStyleBackColor = false;
+            this.btnQuitter.Click += new System.EventHandler(this.btnQuitter_Click);
+            // 
+            // btnLister
+            // 
+            this.btnLister.BackColor = System.Drawing.Color.PeachPuff;
+            this.btnLister.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLister.Location = new System.Drawing.Point(636, 104);
+            this.btnLister.Name = "btnLister";
+            this.btnLister.Size = new System.Drawing.Size(75, 46);
+            this.btnLister.TabIndex = 16;
+            this.btnLister.Text = "Lister les clients";
+            this.btnLister.UseVisualStyleBackColor = false;
+            this.btnLister.Click += new System.EventHandler(this.btnLister_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnLister);
+            this.Controls.Add(this.btnQuitter);
+            this.Controls.Add(this.cbbGenre);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtAdresse);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtTelephone);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtPrenom);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNom);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dtgView);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dtgView;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNom;
+        private System.Windows.Forms.TextBox txtPrenom;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTelephone;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtAdresse;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.ComboBox cbbGenre;
+        private System.Windows.Forms.Button btnQuitter;
+        private System.Windows.Forms.Button btnLister;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GestionCouture.Model;
+using System.Data.SqlClient;
+
+namespace GestionCouture
+{
+    public partial class Form1 : Form
+    {
+        SqlDataAdapter sda;
+        SqlCommandBuilder scb;
+        DataTable dt;
+
+        SqlConnection con = new SqlConnection("Data Source=GOAT;Initial Catalog=bdCoutureDITI;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        Personne pers = new Personne();
+        Client client = new Client();
+
+        private bdCoutureDITIEntities db = new bdCoutureDITIEntities();
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            pers.nomPers = txtNom.Text.Trim();
+            pers.prenomPers = txtPrenom.Text.Trim();
+            pers.telPers = txtTelephone.Text.Trim();
+            pers.emailPers = txtEmail.Text.Trim();
+            pers.adressePers = txtAdresse.Text.Trim();
+            client.genre = cbbGenre.Text.Trim();
+
+            db.Personne.Add(pers);
+            db.Client.Add(client);
+
+
+            db.SaveChanges();
+
+            txtNom.Text = string.Empty;
+            txtPrenom.Text = string.Empty;
+            txtTelephone.Text = string.Empty;
+            cbbGenre.Text = string.Empty;
+            txtEmail.Text = string.Empty;
+            txtAdresse.Text = string.Empty;
+            MessageBox.Show("Insertion réussie");
+        }
+
+
+        private void btnQuitter_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void dtgView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void txtNom_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLister_Click(object sender, EventArgs e)
+        {
+
+            sda = new SqlDataAdapter(@"SELECT prenomPers,nomPers,genre,adressePers,telPers,emailPers  FROM Personne t1 JOIN Client t2 ON t1.idPers = t2.idPers ORDER BY t1.idPers;", con);
+            dt = new DataTable();
+            sda.Fill(dt);
+            dtgView.DataSource = dt;
+            this.dtgView.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+        }
+    }
+}
+
